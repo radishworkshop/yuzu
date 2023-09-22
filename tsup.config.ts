6059cd30376b7253bfe4fe8c040ade147bd3e1ef
Zack Reneau-Wedeen <z.reneau.wedeen@gmail.com>
2023-09-22 11:55:04 -0700

    Remove index.js dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ["src/index.ts", "src/cli.ts"],
+  entry: ["src/cli.ts"],
   format: ["cjs"],
   sourcemap: true,
   target: "esnext",
